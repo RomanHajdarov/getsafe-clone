9fd5617ea7cc0450dcf064f9a9de99aca6006be2
RomanHajdarov <r.hajdarov@hellogetsafe.com>
2023-01-24 13:02:41 +0100

    JS Beschreibung

--- drag-and-drop.js
+++ drag-and-drop.js
@@ -2,7 +2,8 @@ function enableDragAndDrop() {
     let images = document.querySelectorAll('.draggable');
     images.forEach(image => {
       image.addEventListener('dragstart', dragStart);
-      image.addEventListener('dragend', dragEnd);
+      image.addEventListener('dragend', dragEnd);       /* aktiviert das Drag-and-Drop-Verhalten für Bilder (die mit der Klasse "draggable" markiert sind)
+                                                         und Slots (die mit der Klasse "slot" markiert sind) */
     });
     
     let slots = document.querySelectorAll('.slot');
@@ -15,25 +16,28 @@ function enableDragAndDrop() {
   }
 
   let originalSibling;
-  function dragStart() {
+  function dragStart() {                               /* dragStart() wird aufgerufen, wenn ein Benutzer mit der 
+                                                      Maus auf ein Bild klickt und es ziehen will. */
     this.classList.add('dragging');
     originalSibling = this.nextSibling;
   }
   
-  function dragEnd() {
-    this.classList.remove('dragging');
-  }
+  function dragEnd() {                                 /* dragEnd() wird aufgerufen, wenn ein Benutzer die Maustaste loslässt,
+                                                       um das Ziehen des Bilds zu beenden. Es entfernt die Klasse "dragging" */
+ this.classList.remove('dragging');
+  }                                                   
   
-  function dragOver(e) {
+  function dragOver(e) {                                /* Die Funktion dragOver(e) wird aufgerufen, wenn ein Benutzer ein Bild über einen Slot zieht */
     e.preventDefault();
   }
   
-  function dragEnter(e) {
-    e.preventDefault();
-    this.classList.add('drag-over');
+  function dragEnter(e) {                               
+  function dragEnter(e) {                               /* dragEnter(e) wird aufgerufen, wenn ein Benutzer ein Bild in den Bereich eines Slots bewegt. */
+    e.preventDefault();                                  /* verhindert das Standardverhalten des Browsers */
+    this.classList.add('drag-over');                   
   }
   
-  function dragLeave() {
+  function dragLeave() {                                 /* dragLeave() wird aufgerufen, wenn ein Benutzer ein Bild außerhalb des Bereichs eines Slots bewegt */
     this.classList.remove('drag-over');
   }
   
@@ -45,5 +49,5 @@ function enableDragAndDrop() {
     let currentSlot = currentDraggable.parentNode;
     let targetSlot = this;
     currentSlot.appendChild(targetSlot.firstElementChild);
-    targetSlot.appendChild(currentDraggable);
+    targetSlot.appendChild(currentDraggable);               /* dragDrop() wird aufgerufen, wenn ein Benutzer das Bild loslässt, während es sich über einem Slot befindet. Es entfernt die Klasse "drag-over"  */
 }
